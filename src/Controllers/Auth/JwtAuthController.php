303d15af9dd36548b81eca77898403677d79c1af
Juan Sanchez <juan140493@gmail.com>
2018-06-19 09:37:22 -0600

    Modified names of authentication controllers

--- src/Controllers/Auth/JwtAuthController.php
+++ src/Controllers/Auth/JwtAuthController.php
@@ -7,7 +7,7 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-class JwtApiController extends BaseController
+class JwtAuthController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     /**
